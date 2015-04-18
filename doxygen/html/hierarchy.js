e906cb925becc0d119b0f48fc003e8176196c35b
iperius93 <209180@student.pwr.edu.pl>
2015-04-18 19:35:04 +0200

    Revert "Revert "Revert "Signed-off-by: serek8 <serek8@gmail.com>"""
    This reverts commit 81343b9eff1e03d4b963f4f545fb413752d28874.

--- doxygen/html/hierarchy.js
+++
@@ -1,9 +0,0 @@
-var hierarchy =
-[
-    [ "DataFrame", "class_data_frame.html", [
-      [ "MyBenchmark", "class_my_benchmark.html", [
-        [ "MultiplyByTwo", "class_multiply_by_two.html", null ]
-      ] ],
-      [ "NumberGenerator", "class_number_generator.html", null ]
-    ] ]
-];
